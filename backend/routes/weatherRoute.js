e2c7e21d7977549443f2f59c320266b3df40de9b
Mradul999 <demorgans007@gmail.com>
2025-04-12 02:20:01 +0530

    commit

---
+++ backend/routes/weatherRoute.js
@@ -0,0 +1,4 @@
+export const getWeather = async () => {
+  try {
+  } catch (error) {}
+};
--- backend/server.js
+++ backend/server.js
@@ -1,17 +1,16 @@
 import express from "express";
-const app=express();
+const app = express();
 import dotenv from "dotenv";
+import { getWeather } from "./routes/weatherRoute.js";
 dotenv.config();
 app.use(express.json());
 
-const port=process.env.PORT||4001;
+const port = process.env.PORT || 4001;
 
+//routes
 
+app.post("/weather", getWeather);
 
-
-
-
-app.listen(port,()=>{
-    console.log(`Server is running on port ${port}`);
-})
-
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
